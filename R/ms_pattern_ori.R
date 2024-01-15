8b6d52af33f1836c5a3f7f197cf2c8c3475baa1a
Elepdev <63965471+elepdev@users.noreply.github.com>
2024-01-15 11:52:02 +0000

    Update ms_pattern_ori.R

--- R/ms_pattern_ori.R
+++ R/ms_pattern_ori.R
@@ -19,7 +19,7 @@
 #'                 or 'lowBest'
 #' @return  the type of pattern
 #'
-#' @references{\url{https://local.disia.unifi.it/stefanini/RESEARCH/coneu/tutorial-conv.html}}
+#' @references{\url{https://www.eurofound.europa.eu/system/files/2022-04/introduction-to-the-convergeu-package-0.6.4-tutorial-v2-apr2022.pdf}}
 #'
 #'
 #'
