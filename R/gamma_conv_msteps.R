b4ef3e3b809ad4a8fb8819f7d5d2afebbe1cb631
Elepdev <63965471+elepdev@users.noreply.github.com>
2024-01-15 11:49:59 +0000

    Update gamma_conv_msteps.R

--- R/gamma_conv_msteps.R
+++ R/gamma_conv_msteps.R
@@ -12,7 +12,7 @@
 #' @return  dataset of gamma values  (indicated as KIt in Eurofound 2018 paper).
 #'
 #'
-#' @references{\url{https://local.disia.unifi.it/stefanini/RESEARCH/coneu/tutorial-conv.html}}
+#' @references{\url{https://www.eurofound.europa.eu/system/files/2022-04/introduction-to-the-convergeu-package-0.6.4-tutorial-v2-apr2022.pdf}}
 #'
 #'
 #' @examples
