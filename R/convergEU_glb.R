dddd323962319f24c233f67df0cb86811a27a37c
Elepdev <63965471+elepdev@users.noreply.github.com>
2024-01-15 11:46:14 +0000

    Update convergEU_glb.R

--- R/convergEU_glb.R
+++ R/convergEU_glb.R
@@ -15,7 +15,7 @@
 #'          (neighboring countries). The component "metaEUstat" contains the indicators'
 #'          information, while the component "paralintags" is for defining patterns for the Member States.
 #'
-#' @references{ \url{https://local.disia.unifi.it/stefanini/RESEARCH/coneu/tutorial-conv.html}}
+#' @references{ \url{https://www.eurofound.europa.eu/system/files/2022-04/introduction-to-the-convergeu-package-0.6.4-tutorial-v2-apr2022.pdf}}
 #'
 #' @examples
 #'
